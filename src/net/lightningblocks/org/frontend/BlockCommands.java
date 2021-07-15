8f592a9bc0d588efeafc0a1af78739d7d2c42017
Bahhzinga <jackcarter.contact@gmail.com>
2021-07-15 16:15:35 +0100

    First Commit

--- src/net/lightningblocks/org/frontend/BlockCommands.java
+++ src/net/lightningblocks/org/frontend/BlockCommands.java
@@ -1,10 +1,14 @@
 package net.lightningblocks.org.frontend;
 
 import net.md_5.bungee.api.chat.ClickEvent;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class BlockCommands implements CommandExecutor {
 
@@ -15,11 +19,14 @@ public class BlockCommands implements CommandExecutor {
 
             if (label.equalsIgnoreCase("lblocks")) {
                 if (player.hasPermission("lightningblocks.admin")) {
-                }
-                if (args[0].equalsIgnoreCase("give")) {
 
-                } else {
+                    if (args[0].equalsIgnoreCase("give")) {
 
+                        ItemStack lb = new ItemStack(Material.SEA_LANTERN);     ItemMeta meta = lb.getItemMeta();
+                        meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', "&bLightning &3Block"));  lb.setItemMeta(meta);
+
+                        player.getInventory().addItem(lb);
+                    }
                 }
             }
         }
--- src/net/lightningblocks/org/frontend/BlockEvents.java
+++ src/net/lightningblocks/org/frontend/BlockEvents.java
@@ -1,6 +1,37 @@
 package net.lightningblocks.org.frontend;
 
+import net.lightningblocks.org.backend.BlocksFile;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
 
 public class BlockEvents implements Listener {
+
+    @EventHandler
+    public void onLightBlockPlace(BlockPlaceEvent event){
+
+        Player player = event.getPlayer();
+        if (player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.translateAlternateColorCodes('&', "&bLightning &3Block"))){
+            if (player.hasPermission("lightningblock.place")){
+
+                Integer i = null;
+                for (String lb : BlocksFile.get().getConfigurationSection("lightningblocks").getKeys(false)){
+                    i++;
+
+                }
+                BlocksFile.get().set("lightningblocks." + i + ".world", event.getBlock().getLocation().getWorld().getName());
+                BlocksFile.get().set("lightningblocks." + i + ".x", event.getBlock().getLocation().getBlockX());
+                BlocksFile.get().set("lightningblocks." + i + ".y", event.getBlock().getLocation().getBlockY());
+                BlocksFile.get().set("lightningblocks." + i + ".z", event.getBlock().getLocation().getBlockZ());
+                BlocksFile.get().set("lightningblocks." + i + ".interval", 15);
+                BlocksFile.save();
+
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&3&lLightning&b&lBlocks &8| &7You added a new Lightning Block!"));
+
+            }
+        }
+    }
+
 }
